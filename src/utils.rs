cb4f162cfdb40b754798f49c737020fa1aaec452
dshehbaj <shehbaj.dhillon@gmail.com>
2021-04-29 01:19:50 -0700

    Completed utils.

--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,38 @@
 use std::io::{stdout, Write};
 
-pub fn print_grid(grid: &Vec<Vec<i32>>) {
-    io::stdout().flush();
+
+pub fn print_grid(grid: &Vec<Vec<i32>>, size: i32) {
+    stdout().flush().expect("Error while flushing stdout");
     for row in grid {
         print!(" ");
+        stdout().flush().expect("Error while flushing stdout");
         for num in row {
-            print!("{} |", num as char);
+            let temp = *num as u8;
+            print!("{} | ", temp as char);
         }
+        stdout().flush().expect("Error while flushing stdout");
+        println!();
+        for _i in 0..size {
+            print!("----");
+        }
+        stdout().flush().expect("Error while flushing stdout");
+        println!();
     }
 }
+
+
+pub fn translate(num: i32) -> i32 {
+    if num == 7 || num == 8 || num == 9 {
+        return num - 7;
+    }
+    else if num == 4 || num == 5 || num == 6 {
+        return num - 1;
+    }
+    else if num == 1 || num == 2 || num == 3 {
+        return num + 5;
+    }
+    else {
+        return -1;
+    }
+}
+
