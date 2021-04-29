2eeb80ae58a1fa7aee06cd9cbe86e5c55d991af5
dshehbaj <shehbaj.dhillon@gmail.com>
2021-04-29 01:23:16 -0700

    Completed Player class.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 mod board;
+mod player;
+mod utils;
 
 fn main() {
-    println!("Hello, world!");
+    let board: board::Board = board::Board::new(3, 32);
+    utils::print_grid(&board.get_grid(), board.get_size());
+    println!("Yo");
 }
